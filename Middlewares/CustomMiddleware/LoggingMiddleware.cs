dbc2bd112cc0cfb71f02bd2e215073b1a647eab8
Marimuthu B <Marimuthu.B@ntgfreight.com>
2024-11-20 17:07:14 +0530

    Middleware learning

---
+++ Middlewares/CustomMiddleware/LoggingMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace Middlewares.CustomMiddleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context) 
+        {
+            Console.WriteLine("Handling request: "+ context.Request.Path);
+            await _next(context);
+            Console.WriteLine("Finished handling request.");
+        }
+    }
+}
